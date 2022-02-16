5305d4073e9a2ff618bf7378bddb1364c59da1dc
Ackerman, Adam P <adamackrmn@gmail.com>
2022-02-15 22:05:59 -0600

    checking in code for wpf json from file example

---
+++ In_Class_Examples/WPF-JSONFromFile/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSONFromFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string json = File.ReadAllText("MOCK_DATA.json");
+
+            List<Person> peeps = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            //lstPeeps.ItemsSource = peeps;
+
+            foreach (Person person in peeps)
+            {
+                if (person.city.ToUpper()[0] == 'R')
+                {
+                    lstPeeps.Items.Add(person);
+
+                }
+            }
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string serializedData = JsonConvert.SerializeObject(lstPeeps.Items, Formatting.Indented);
+
+            File.WriteAllText("filteredData.json", serializedData);
+        }
+    }
+}
