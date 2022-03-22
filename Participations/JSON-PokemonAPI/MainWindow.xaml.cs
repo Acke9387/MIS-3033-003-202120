52a20b36ab729f25c34ccaacb5a11f65687e666e
Ackerman, Adam P <adamackrmn@gmail.com>
2022-03-22 09:54:06 -0500

    Completed the pokemon api participation

---
+++ Participations/JSON-PokemonAPI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_PokemonAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+
+                string json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1200").Result;
+
+                AllPokemonAPI api = JsonConvert.DeserializeObject<AllPokemonAPI>(json);
+
+                cboPokemon.ItemsSource = api.results;
+
+                //api.results.ForEach( poke => cboPokemon.Items.Add(poke));
+                //foreach (var poke in api.results)
+                //{
+                //    cboPokemon.Items.Add(poke);
+                //}
+            }
+        }
+
+        private void cboPokemon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            Pokemon pokemon = cboPokemon.SelectedItem as Pokemon;
+            PokemonInfoWIndow info = new PokemonInfoWIndow();
+
+            info.PopulateData(pokemon);
+
+            info.ShowDialog();
+
+
+        }
+
+
+    }
+}
---
+++ Participations/JSON-PokemonAPI/PokemonInfoWIndow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JSON_PokemonAPI
+{
+    /// <summary>
+    /// Interaction logic for PokemonInfoWIndow.xaml
+    /// </summary>
+    public partial class PokemonInfoWIndow : Window
+    {
+        PokemonInfo info = null;
+        string status = "";
+        public PokemonInfoWIndow()
+        {
+            InitializeComponent();
+        }
+
+        public void PopulateData(Pokemon pokemon)
+        {
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(pokemon.url).Result;
+
+                info = JsonConvert.DeserializeObject<PokemonInfo>(json);
+
+                lblTitle.Content = info.name;
+                txtHeight.Text = info.height.ToString("N");
+                txtWeight.Text = info.weight.ToString("N");
+                imgPic.Source = new BitmapImage(new Uri(info.sprites.front_default));
+                status = "back";
+                //imgPic.Tag = "back";
+            }
+
+        }
+
+        private void btnDance_Click(object sender, RoutedEventArgs e)
+        {
+            if (status == "back")
+            {
+                imgPic.Source = new BitmapImage(new Uri(info.sprites.back_default));
+                status = "front";
+            }
+            else
+            {
+                imgPic.Source = new BitmapImage(new Uri(info.sprites.front_default));
+                status = "back";
+            }
+        }
+    }
+}
